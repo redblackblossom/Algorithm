f16cbb8ccdb13fdf456baef3650fceea2d8687c1
조경호 <155342235+redblackblossom@users.noreply.github.com>
2025-08-05 16:02:37 +0900

    [Silver III] Title: N과 M (4), Time: 812 ms, Memory: 28312 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class Main {
+	
+	static int[] ch;
+	static int[] output;
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int m = sc.nextInt();
+		
+		ch = new int[n];
+		output = new int[m];
+		
+		
+		DFS(0,0,n,m);
+	}
+	
+	static void DFS(int start, int depth, int n, int r) {
+		if(depth == r) {
+			
+			for(int i =0; i<output.length; ++i) {
+				System.out.print(output[i] + " ");
+			}
+			System.out.println();
+			
+			return;
+		}
+		
+		for(int i =start; i<n;++i) {
+			output[depth] = i+1;
+			DFS(i, depth+1, n,r);
+		}
+	}
+	
+}
