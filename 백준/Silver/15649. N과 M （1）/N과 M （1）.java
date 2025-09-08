acf2da1d9bd72824a5cda612998e7107bc0e4077
조경호 <155342235+redblackblossom@users.noreply.github.com>
2025-09-08 17:05:57 +0900

    [Silver III] Title: N과 M (1), Time: 1680 ms, Memory: 63184 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,42 +1,35 @@
 import java.util.*;
 
 public class Main {
-	
+
 	static int[] ch;
 	static int[] output;
-	
+
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
 		int m = sc.nextInt();
+
 		ch = new int[n];
 		output = new int[m];
-		
-		DFS(0,n,m);
-		
+		DFS(n,m,0);
 	}
-	
-	
-	static void DFS(int depth, int n, int r) {
+
+	static void DFS(int n, int r, int depth) {
 		if(depth == r) {
-			for(int i =0; i<output.length; ++i) {
+			for(int i = 0; i < r; i++) {
 				System.out.print(output[i] + " ");
 			}
 			System.out.println();
-			return;
-		}
-		
-		for(int i =0; i<n;++i) {
-			if(ch[i] ==0) {
-				ch[i] = 1;
-				output[depth] = i + 1;
-				
-				DFS(depth +1, n,r);
-				ch[i] = 0;
-				
+		} else {
+			for(int i=0; i<n;++i) {
+				if(ch[i] == 0) {
+					ch[i] = 1;
+					output[depth] = i +1;
+					DFS(n,r,depth+1);
+					ch[i] = 0;
+				}
 			}
 		}
 	}
-	
-
 }
