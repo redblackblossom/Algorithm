6ef606425ba269c396f0d6bf34f345a33a040daf
조경호 <155342235+redblackblossom@users.noreply.github.com>
2026-05-02 11:35:00 +0900

    [level 1] Title: 과일로 만든 아이스크림 고르기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/1/133025. 과일로 만든 아이스크림 고르기/과일로 만든 아이스크림 고르기.sql
@@ -0,0 +1,7 @@
+-- 코드를 입력하세요
+SELECT f.FLAVOR
+from FIRST_HALF as f
+join ICECREAM_INFO as i 
+on f.FLAVOR = i.FLAVOR
+where f.TOTAL_ORDER > 3000 and i.INGREDIENT_TYPE = "fruit_based"
+order by f.TOTAL_ORDER DESC
